ea682f804a580548221cc989daac81153c337288
Jonathan Sanchez <jonsanchezu@gmail.com>
2017-07-01 15:38:48 -0700

    added feedback messages

--- main.py
+++ main.py
@@ -15,6 +15,42 @@ def index():
     template = jinja_env.get_template('home.html')
     return template.render()
 
+@app.route("/", methods = ['POST'])
+def validate_info():
+    username = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify_password']
+    email = request.form['email']
+
+    username_error = ''
+    password_error = ''
+    verify_error = ''
+    email_error = ''
+
+    if " " in username or len(username) < 3 or len(username) > 20:
+        username_error = "That's not a valid username"
+
+    if " " in password or len(password) < 3 or len(password) > 20:
+        password_error = "That's not a valid password"
+
+    if password != verify_password:
+        verify_error = "Passwords don't match"
+
+    if "@" not in email or "." not in email or email.count(".") > 1 or email.count("@") > 1 or " " in email or len(email) < 3 or len(email) > 20:
+        email_error = "That's not a valid email"
+
+    if not username_error and not password_error and not verify_error and not email_error:
+        return "hello"
+
+    else:
+        template = jinja_env.get_template('home.html')
+        return template.render(username=username,
+            email = email,
+            username_error=username_error,
+            password_error = password_error,
+            verify_error = verify_error,
+            email_error = email_error)
+
 @app.route("/welcome", methods=['POST'])
 def welcome():
     username = request.form['username']
